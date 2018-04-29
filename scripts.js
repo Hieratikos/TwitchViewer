3bc0d6ca845a3032acab1a52cfd60c5ac9e9ee03
Hieratikos <jordicombs@yahoo.com>
2018-04-29 16:47:04 -0700

    Added col-sm-4 and col-xs-4 to the display <div> tags

--- scripts.js
+++ scripts.js
@@ -76,8 +76,8 @@ $(document).ready(function () {
     function printToScreen(logo, status, name){
         $("#followerInfo").prepend(
             "<div class='row'>" +
-            "<div class='col-md-4'>" + "<img class='twitchImg' src='" + logo + "'>" + "</div>" +
-            "<div class='col-md-4'>" + name + "</div>" +
-            "<div class='col-md-4'>" + status + "</div></div>");
+            "<div class='col-md-4 col-sm-4 col-xs-4'>" + "<img class='twitchImg' src='" + logo + "'>" + "</div>" +
+            "<div class='col-md-4 col-sm-4 col-xs-4'>" + name + "</div>" +
+            "<div class='col-md-4 col-sm-4 col-xs-4'>" + status + "</div></div>");
     }
 });
